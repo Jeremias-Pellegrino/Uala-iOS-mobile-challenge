6b0e2ada22eab46e01c6f2b87b3144bf1a266f1a
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2024-11-22 22:29:11 -0300

    Initial Commit

---
+++ Uala-iOS-challenge/Uala_iOS_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uala_iOS_challengeApp.swift
+//  Uala-iOS-challenge
+//
+//  Created by Jeremias on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Uala_iOS_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Uala-iOS-challengeTests/Uala_iOS_challengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Uala_iOS_challengeTests.swift
+//  Uala-iOS-challengeTests
+//
+//  Created by Jeremias on 22/11/2024.
+//
+
+import Testing
+@testable import Uala_iOS_challenge
+
+struct Uala_iOS_challengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
