42131a6e2b9eff4014b8e0276d64d2d9c005a305
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2024-11-22 22:57:30 -0300

    Added city decodable

---
+++ Uala-iOS-challenge/City.swift
@@ -0,0 +1,23 @@
+//
+//  City.swift
+//  Uala-iOS-challenge
+//
+//  Created by Jeremias Pellegrino on 22/11/2024.
+//
+
+import SwiftUI
+
+struct City: Codable {
+    
+    static let dummy: City = City(coord: Coord(lon: 0, lat: 0), country: "Country", _id: 999, name: "city name")
+    
+    let coord: Coord
+    let country: String
+    let _id: Int
+    let name: String
+}
+
+struct Coord: Codable {
+    let lon: Float
+    let lat: Float
+}
