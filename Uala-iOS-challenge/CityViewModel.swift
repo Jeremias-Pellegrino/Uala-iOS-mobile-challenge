c2cf01f6586a3e704933682efcd2c0281d8e66ee
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2024-11-24 18:27:46 -0300

    Improving Trie approach

--- Uala-iOS-challenge/CityViewModel.swift
+++ Uala-iOS-challenge/CityViewModel.swift
@@ -10,67 +10,62 @@ import Combine
 
 class CityViewModel: ObservableObject {
     var repository: Repository = Repository()
-    var entity: Entity = Entity()
-    
     @State private var trie = ObservableTrie()
     
+    private var allCities: [String: City] = [:]
+    private var initialCities: [City] = []
+    
+    @Published var favorites: [City] = []
     @Published var showFavorites: Bool = false
-    var favorites: [City] = []
     
     @Published var searchText: String = ""
-    
     @Published var searchResults: [City] = []
     
-    private var allCities: [String: City] = [:]
-    
+    @Published var batchSize = 100
     @Published var loading: Bool = true
-    
-    @State var currentPage = 0
     
     private var cancellables = Set<AnyCancellable>()
     
+    func setupCities() {
+        
+    }
+    
     init() {
-        print("initttt")
         Task {
-            //Mientras esperamos, mostramos el banner con el spinner cargando
             try await fetch()
+            setupCities()
         }
         
         trie.$searchResults
             .sink { [weak self] cityNames in
                 self?.filterCities(matching: cityNames)
-                
             }
             .store(in: &cancellables)
         
         $searchText
-//            .debounce(for: .seconds(0.1), scheduler: RunLoop.main)
             .sink { word in
-                if word == "" { return }
+                if word == "" {
+                    self.searchResults = self.initialCities
+                    return
+                }
                 self.trie.searchWithPrefix(word)
             }
             .store(in: &cancellables)
     }
     
     private func filterCities(matching cityNames: [String]) {
-        
         var res = [City]()
-        
         for name in cityNames {
-            
             res.append(allCities[name]!)
         }
         res = res.sorted { $0.name < $1.name }
-        res = Array(res.prefix(100))
+        res = Array(res.prefix(batchSize))
         searchResults = res
     }
     
     func fetch() async throws {
-        print("fetching")
-        
-        
         let data = try await repository.apiClient.requestData(with: repository.url)
-        DispatchQueue(label: "dwnld", qos: .background).async {
+        DispatchQueue(label: "dwnld", qos: .userInitiated).async {
             do {
                 var cities: [City] = []
                 cities = try Parser().parse(data: data, to: [City].self)
@@ -87,69 +82,13 @@ class CityViewModel: ObservableObject {
                     }
                 }
             } catch {
+                //Display an alert
                 print(error)
             }
         }
-       
-//        let first100 = Array(cities.prefix(100))
-        
-        //TODO: parsear las cities en el diccionario de un saque, en el mismo modelo asi no hago el laburo de recorrer 200k items dos veces.
-        
-        
-        //feed trie
-     
-        
-//        await MainActor.run {
-//            entity.cities = first100
-//        }
     }
-    
-//    func itemsToRender() -> [String] {
-//        var items: [String] = []
-//        for i in 0...currentPage {
-//            items.append(contentsOf: trie.searchResults[i])
-//        }
-//        
-//        return items
-//    }
-    
     
     func checkForFavorites() {
-        //TODO: adasdasd
-    }
-    
-//    func searchWithPrefix(_ prefix: String) {
-//        
-//        let results = trie.searchResults(for: prefix)
-//        print(results)
-//        currentPage = 0
-//    }
-    
-    //should be ID
-//    func updatePagination(lastItemID: String) {
-//        print(lastItemName ?? "")
-//        
-//        let pageSize = 30
-//        if lastItemName == trie.searchResults[currentPage][pageSize - 10]  {
-//            if currentPage < trie.searchResults.count {
-//                currentPage += 1
-//            }
-//        }
-//    }
-    
-}
-
-extension CityViewModel {
-    func appendToVisible(_ name: String) {
-        //todo
-    }
-    
-    func removeFavorite(_ index: Int) {
-        favorites.remove(at: index)
-    }
-    
-    func addFavorite(at offsets: IndexSet) {
-        entity.cities.remove(atOffsets: offsets)
-        //update the stored file?
+        
     }
 }
--- Uala-iOS-challenge/Utils.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Utils.swift
-//  Uala-iOS-challenge
-//
-//  Created by Jeremias on 22/11/2024.
-//
-
-import Foundation
