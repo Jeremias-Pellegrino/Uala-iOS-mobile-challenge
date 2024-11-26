5a670e334198c1b4b0d9bbc3d91bacf0dd9a634e
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2024-11-26 04:03:38 -0300

    API Update

--- Uala-iOS-challenge/API/APIClient.swift
+++ Uala-iOS-challenge/API/APIClient.swift
@@ -42,26 +42,18 @@ struct HTTPResponseCode {
 
 class APIClient {
     
-    //TODO: Consider replacing this with NSCache to improve performance
     let cache: URLCache
     
     init() {
-        
-        //TODO: Verify if the size is enough to store the 200k items.
+        //MARK: Could be useful to have a cache around and avoid wasting 20+ mb of download per call. Verify if the size is enough to store the 200k items.
         cache = URLCache(memoryCapacity: 50 * 1024 * 1024,
                          diskCapacity: 200 * 1024 * 1024,
                          diskPath: nil)
     }
     
-    //    deinit {
-    //        cache.removeAllCachedResponses()
-    //    }
-    
     func requestData(with url: URL, shouldCache: Bool = true) async throws -> Data {
         
-        let cacheRef = cache
-        
-        //TODO: May want add check conditions to verify the current data correctnes, etc. For the purposes of this challenge, the data remains constant, so we don't need to.
+        //MARK: May want add check conditions to verify the current data correctnes, etc. For the purposes of this challenge, the data remains constant, so... it's safe to assume we don't need to.
         if let cachedResponse = cache.cachedResponse(for: URLRequest(url: url)) {
             return cachedResponse.data
         }
@@ -69,12 +61,9 @@ class APIClient {
         return try await startDataTask(url: url, shouldCache: shouldCache)
     }
     
-    //TODO: run on background / low priority thread
     func startDataTask(url: URL, shouldCache: Bool = false) async throws -> Data {
+
         let request = URLRequest(url: url)
-        let config = URLSessionConfiguration.ephemeral
-        let session = URLSession(configuration: config)
-        
         let (data, response) = try await URLSession.shared.data(for: request)
         
         guard let validResponse = response as? HTTPURLResponse
@@ -87,11 +76,7 @@ class APIClient {
         if !responseCode.isSatisfactory {
             throw responseCode.category
         }
-        
-//        if shouldCache {
-//            let cachedResponse = CachedURLResponse(response: response, data: data)
-//        }
-        
+
         return data
     }
 }
